908f094a62a4c98fba5c742fed8ee26f660272a9
Cannellespeculos <cjeanpierre@edenschool.fr>
2023-03-20 13:49:35 +0100

    end of the second algo

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 import "./style.css";
 
+
+// première algo
 let characters = "";
 for (let i = 0; i <= 10; i++) {
   characters += i;
 
 }
 console.log(characters)
+
+// deuxième algo 
+let characterspace = "";
+for (let a = 0; a <= 10; a++) {
+  characterspace += a;
+  characterspace += " "
+}
+
+console.log(characterspace)
